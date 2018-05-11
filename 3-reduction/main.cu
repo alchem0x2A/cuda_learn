e24212b1f821dde3687e094063a9fae92f45fc44
lovaulonze <lovaulonzezoeta@gmail.com>
2018-05-11 15:56:20 +0200

    Use pure GPU reduction (2 rounds)

--- 3-reduction/main.cu
+++ 3-reduction/main.cu
@@ -5,7 +5,7 @@
 
 // Reduce from *data (size len) to *result
 template<typename T>
-__global__ void cudaReduceThread(T *data, T *result, int len){
+__global__ void cudaReduceThread(const T *data, T *result, int len){
   extern __shared__ T shared_data[];	//
   int pid = blockIdx.x * blockDim.x + threadIdx.x; // global thread
   int bid = blockIdx.x;
@@ -35,20 +35,29 @@ __global__ void cudaReduceThread(T *data, T *result, int len){
   }
 }
 
-
+// Summation by 2 rounds of CUDA block reduction
 template<typename T>
 T cudaReduction(T *data, int len){
   int N_thread = 1024;
   int N_block = (len + N_thread - 1) / N_thread;
+  int N_thread2 = 1;
+  while (N_thread2 < N_block) N_thread2 *= 2;
   std::cout << "Number of blocks: " << N_block << std::endl;
   float *ddata; cudaMalloc((void **)&ddata, len * sizeof(float));
+
   cudaMemcpy(ddata, data, len * sizeof(float), cudaMemcpyHostToDevice);
+  
   T *block_sum = new T[N_block];
   std::fill(block_sum, &block_sum[N_block], 0.0f);
   T *dblock_sum; cudaMalloc((void **)&dblock_sum, N_block * sizeof(T));
+
   cudaMemcpy(dblock_sum, block_sum, N_block * sizeof(T),
 	     cudaMemcpyHostToDevice);
+
+  // First run on thread
   cudaReduceThread<<<N_block, N_thread, N_thread * sizeof(T)>>>(ddata, dblock_sum, len);
+  // Second run on block
+  cudaReduceThread<<<1, N_thread2, N_thread2 * sizeof(T)>>>(dblock_sum, dblock_sum, N_block);
   cudaError_t err = cudaGetLastError();
   if (err != cudaSuccess)
     {
@@ -60,14 +69,14 @@ T cudaReduction(T *data, int len){
 
   cudaMemcpy(block_sum, dblock_sum, N_block * sizeof(T),
 	     cudaMemcpyDeviceToHost);
-  T host_sum = 0.0;
-  std::cout << "Sum each block:" << std::endl;
-  for (int i = 0; i < N_block; ++i){
-    std::cout << block_sum[i] << " ";
-  }
-  std::cout << std::endl;
-  for (int i = 0; i < N_block; ++i)
-    host_sum += block_sum[i];
+  T host_sum = block_sum[0];
+  // std::cout << "Sum each block:" << std::endl;
+  // for (int i = 0; i < N_block; ++i){
+    // std::cout << block_sum[i] << " ";
+  // }
+  // std::cout << std::endl;
+  // for (int i = 0; i < N_block; ++i)
+    // host_sum += block_sum[i];
   delete block_sum;
   cudaFree(dblock_sum); cudaFree(ddata);
   return host_sum;
